314fb0159a8b55dc1e61d83e0738ef5fcda23424
shibaiyong <shibaiyong@example.com>
2020-05-27 17:10:19 +0800

    shibaiyong change

--- src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';   // 中间件，有了这个就可以支持异步action
-
-import RootReducer from './reduces/index'
-
-const store = createStore(RootReducer, applyMiddleware(ReduxThunk));
-
-export default store;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux'
-import action from './actions'
+// import action from './actions'
 
 var copystate={
   num:0,
@@ -7,16 +7,17 @@ var copystate={
 }
 
 //面向对象的编程方法，createStore相当于构造函数。而且reducer是纯函数。
-var reducer = (state={  //注意传参数的方式
+ //不建议直接修改state,可以采用 ... 或者 Object.assign()
+ //注意传参数的方式
+var reducer = (state={  
   num:0, text:'hello word11'},action)=>{
   switch(action.type) {
     case 'changenum':
-      copystate.num++;  //不建议直接修改state,可以采用 ... 或者 Object.assign()
-      return Object.assign({},state,{num:copystate.num})
-      break;
+      copystate.num++; 
+      return Object.assign({},state,{num:copystate.num}); 
+
     case 'changetext':
-      return Object.assign({},state,{text:action.text})
-      break;
+      return Object.assign({},state,{text:action.text});
     default :
       return state;
   }
