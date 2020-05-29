e252acc021fa0753d4b251cf577a8e29109293a6
shibaiyong <shibaiyong@example.com>
2020-05-29 16:44:09 +0800

    shibaiyong change

--- src/components/logins/Login.js
+++ src/components/logins/Login.js
@@ -1,45 +1,77 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { List, InputItem, WhiteSpace } from 'antd-mobile';
-
+import { List, InputItem,Toast } from 'antd-mobile';
+import { createForm } from 'rc-form';
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props)
-    this.headerstyle = {
-    }
-    this.state = {
-
-    }
-
-    this.unsubscribe = null
-
-    this.submitForm = this.submitForm.bind(this)
-
+  
+  submitForm = () => {
+    this.props.form.validateFields({ force: true }, (error) => {
+      if (!error) {
+        console.log(this.props.form.getFieldsValue());
+      } else {
+        console.log(this.props.form.getFieldError('tel'));
+      }
+    })
   }
 
-  submitForm(event) {
-    //非受控组件表单提交
-    let username = this.username.value //等同于 focusInput 中 this.textInput.current = this.username
-    let password = this.password.value
-    let remember = this.remember.checked
-    this.loginForm = { username, password, remember }
-    console.log(this.loginForm)
+  validatePassword(rule, value, callback){
+    if (value && value.length >=8) {
+      callback();
+    } else if(value.length===0){
+      callback(new Error('请输入密码'));
+    } else {
+        callback(new Error('请输入至少8位密码'));
+    }
   }
+
+
+  validateTel(rule, value, callback){
+    if (value && value.length===13) {
+        callback();
+    } else if(value.length===0){
+        callback(new Error('请输入电话号码'));
+    } else {
+        callback(new Error('电话号码不合法'));
+    }
+}
+
+  
 
   componentDidMount() {
   }
   componentWillUnmount() {
   }
   render() {
+    const { getFieldProps, getFieldError } = this.props.form
     return (
       <div className='login'>
         <ul>
           <li>
-            <label>UserName&nbsp;:&nbsp;&nbsp;</label>
-            <InputItem clear ref={input => this.username = input}></InputItem>
+            
+            <List>
+              <InputItem {...getFieldProps('password', {
+                            rules: [
+                            { validator: this.validatePassword }
+                            ],
+                        })}
+                        onErrorClick={() => {
+                          Toast.info(getFieldError('password'), 1)
+                          }}
+                          >Password:</InputItem>
+              <InputItem {...getFieldProps('tel', {
+                            rules: [
+                            { validator: this.validateTel}
+                            ],
+                        })}
+                        onErrorClick={() => {
+                          Toast.info(getFieldError('tel'), 1)
+                          }}
+                        type="phone">Phone:</InputItem>
+            </List>
+            
             </li>
-          <li><label>Password&nbsp;:&nbsp;&nbsp;</label><input ref={input => this.password = input} type="text" className='shi-input' /></li>
+          
           <li><input type="submit" className='shi-input' value='登录' onClick={this.submitForm}/></li>
           <li><input type="checkbox" ref={input => this.remember = input}/>记住密码</li>
         </ul>
@@ -48,22 +80,5 @@ class Login extends React.Component {
     )
   }
 }
-//需要渲染什么数据
-function mapStateToProps(state) {
-  return {
-    num: state.num,
-    text: state.text
-  }
-}
-//需要触发什么行为
-function mapDispatchToProps(dispatch) {
-  return {
-    // changeNum: () => dispatch({ type: 'changenum', num: num }),
-    changeText: (e) => {
-      var val = e.target.value
-      dispatch({ type: 'changetext', text: val })
-    }
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default createForm()(Login)
